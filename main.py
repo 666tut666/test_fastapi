2c2a982af73a24ce5d7f7c014404083fabbc18b7
666tut666 <666tut666@users.noreply.github.com>
2022-11-16 15:19:12 +0545

    test

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from config import setting
 from database import engine
 from models import Base
 from routers import users, items, login
+from webapps.routers import items as web_items, users as web_users, auth as web_auth
+from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
 
@@ -21,7 +23,7 @@ tags = [
         "description": "order related route"
     }
 ]
-##tags= [..{},..] diff tags banaucha
+##tags= [{...}, {...}] diff tags banaucha
 
 app = FastAPI(
     title=setting.TITLE,
@@ -34,6 +36,13 @@ app = FastAPI(
 )
 
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+    #mount the path for img
+
+
 app.include_router(users.router)
 app.include_router(items.router)
 app.include_router(login.router)
+app.include_router(web_items.router)
+app.include_router(web_users.router)
+app.include_router(web_auth.router)
---
+++ webapps/routers/items.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+from models import Items, User
+from sqlalchemy.orm import Session
+from database import get_db
+
+router = APIRouter(include_in_schema=False)
+templates = Jinja2Templates(directory="templates")
+    #defined path where html files are
+        ##templates.....
+
+
+@router.get("/")
+def item_home(
+        request: Request,
+        db:Session=Depends(get_db),
+        msg:str=None
+):
+    items = db.query(Items).all()
+    return templates.TemplateResponse("item_hp.html", {"request": request, "items": items, "msg": msg})
+        #using pydantic approach
+        ##taking i/p: as request// request type
+            #item_hp.html is item`s home page
+            ##gotta declare Request in Jinja
+        ## as we have used item_hp, gotta define it there too
+
+
+@router.get("/detail/{id}")
+def item_detail(
+        request: Request,
+        id: int,
+        db: Session=Depends(get_db)
+):
+    item = db.query(Items).filter(Items.id==id).first()
+    user = db.query(User).filter(User.id==item.owner_id).first()
+    return templates.TemplateResponse(
+        "item_detail.html",
+        {
+            "request": request,
+            "item": item,
+            "user": user
+        }
+    )
